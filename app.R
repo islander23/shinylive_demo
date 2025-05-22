e04cd20d5f6cc41459f4faf42268e19009106c0a
wen <asmallisland222@gmail.com>
2025-05-22 14:34:05 +0800

    first commit

---
+++ app.R
@@ -0,0 +1,32 @@
+
+# https://hbctraining.github.io/Training-modules/RShiny/lessons/shinylive.html
+
+install.packages("shinylive")
+install.packages("httpuv")
+
+
+library(shinylive)
+library(httpuv)
+
+library(shiny)
+library(ggplot2)
+library(DT)
+
+ui <- fluidPage(
+  plotOutput("plot", brush = "plot_brush"),
+  DTOutput("table")
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot(
+    ggplot(mtcars) +
+      geom_point(aes(x = mpg, y = disp))
+  )
+  output$table <- renderDT({
+    brushedPoints(mtcars, input$plot_brush)
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
